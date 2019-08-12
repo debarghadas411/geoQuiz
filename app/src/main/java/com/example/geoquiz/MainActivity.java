4a237b2b6e5f5146ff344631cbe7a89c4e011454
Debargha Das <debarghadas411@gmail.com>
2019-08-12 22:27:01 +0530

    Who is Arnab Chatterjee

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -2,36 +2,89 @@ package com.example.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button astrologer;
-    private Button professor;
+    private Button button1;
+    private Button button2;
+    private Button next;
+    private TextView tv;
+    private int msgid;
+    private Questions[] QBank=new Questions[]{
+            new Questions(R.string.Q1,true),
+            new Questions(R.string.Q2,true),
+            new Questions(R.string.Q3,true),
+            new Questions(R.string.Q4,true),
+            new Questions(R.string.Q5,true),
+            new Questions(R.string.Q6,true),
+            new Questions(R.string.Q7,true),
+            new Questions(R.string.Q8,true),
+            new Questions(R.string.Q9,true),
+            new Questions(R.string.Q10,true),
+    };
+    private int index=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        astrologer=(Button)findViewById(R.id.b1);
-        astrologer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast k=Toast.makeText(MainActivity.this,R.string.correct,Toast.LENGTH_LONG);
-                k.setGravity(Gravity.TOP,0,0);
-                k.show();
-            }
-        });
-        professor=(Button)findViewById(R.id.b2);
-        professor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                Toast.makeText(MainActivity.this,R.string.wrong,Toast.LENGTH_LONG).show();
+        tv=(TextView)findViewById(R.id.tv);
+        QChange();
+        next=(Button)findViewById(R.id.b3);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                QChange();
             }
         });
+        button1=(Button)findViewById(R.id.b1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ansCheck(true);
+                Toast k=Toast.makeText(MainActivity.this,msgid,Toast.LENGTH_LONG);
+                //k.setGravity(Gravity.TOP,0,0);
+                k.show();
+                QChange();
+            }
+        });
+        button2=(Button)findViewById(R.id.b2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ansCheck(false);
+                Toast.makeText(MainActivity.this,msgid,Toast.LENGTH_LONG).show();
+                QChange();
+            }
+        });
+
+    }
+
+    private void QChange() {
+        if(index==10){
+            //intent
+            Intent k=new Intent(MainActivity.this,Main2Activity.class);
+            startActivity(k);
+        }
+        else{
+            int q=QBank[index++].getTextResId();
+            tv.setText(q);
+        }
+    }
+    private void ansCheck(boolean press){
+        boolean ans=QBank[index-1].isAnswerTrue();
+        if(press==ans){
+            msgid=R.string.correct;
+        }
+        else{
+            msgid=R.string.wrong;
+        }
 
     }
 }
